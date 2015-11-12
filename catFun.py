841519a02093d80b76638a36e9810c93c70c576a
Cody Collins <csc6xz@virginia.edu>
2015-11-12 16:00:46 -0500

    Part C Lab 11/12

--- catFun.py
+++ catFun.py
@@ -102,7 +102,7 @@ def handleEvent(state, event):
 # World state will be single x coordinate at left edge of world
 
 # The cat starts at the left, moving right 
-initState = (250,250,1,1)
+initState = (randint(0,499),randint(0,499),randint(1,5),randint(1,5))
 
 # Run the simulation no faster than 60 frames per second
 frameRate = 60
