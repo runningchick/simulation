e88f706916e0c8eadf52420fbb9c4ca70208c21e
Emma Ward <ecw5zs@virginia.edu>
2015-11-17 16:53:43 -0500

    Added Images of Sun and Earth and reformatted so its centered

--- planets.py
+++ planets.py
@@ -8,22 +8,27 @@ width = 500
 height = 500
 rw.newDisplay(width, height, name)
 
-sun = dw.loadImage("cat.bmp")
-earth = dw.loadImage("cat.bmp")
+def loadImage(filename):
+    imageSurface = pg.image.load(filename)
+    imageSurface.convert()
+    return imageSurface
+
+sun = loadImage("sun2.jpeg")
+earth = loadImage("earth1.jpeg")
 
 def updateDisplay(state):
     dw.fill(dw.black)
-    dw.draw(sun, (250, 250))
+    dw.draw(sun, (200, 200))
     dw.draw(earth, (state[0], state[1]))
 
 def updateState(state):
-    if (200 <= state[1] < 300 and state[0] == 200):
+    if (50 <= state[1] < 350 and state[0] == 50):
         y_down_velocity = 1
         location = (state[0]+state[2], state[1]+y_down_velocity, state[2], state[3])
-    elif (state[1] == 300 and 200 <= state[0] < 300):
+    elif (state[1] == 350 and 50 <= state[0] < 350):
         x_right_velocity = 1
         location = (state[0]+x_right_velocity, state[1]+state[3], state[2], state[3])
-    elif (200 < state[1] <= 300 and state[0] == 300):
+    elif (50 < state[1] <= 350 and state[0] == 350):
         y_up_velocity = -1
         location = (state[0]+state[2], state[1]+y_up_velocity, state[2], state[3])
     else: 
@@ -41,7 +46,7 @@ def handleEvent(state, event):
     print(event)
     return(state)
     
-initState = [200, 200, 0, 0]
+initState = [50, 50, 0, 0]
 
 frameRate = 60
 
